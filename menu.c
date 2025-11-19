0612789a8aeb0f6a44c4f45ab7da7372cc4b4e5e
Nany Gomes <lgs25@aluno.ifal.edu.br>
2025-11-19 17:45:24 -0300

    modificações2

--- menu.c
+++ menu.c
@@ -23,9 +23,9 @@ void menuInicial() {
             case 1: {
                 int pos = login();
                 if (pos != -1) {  
-                    if (strcmp(users[pos].role, "gerente") == 0) {
-                        menuGerente(pos);
-                    } else if (strcmp(users[pos].role, "atendente") == 0) {
+                    if (strcmp(users[pos].cargo, "gerente") == 0) {
+                        menuGerente();
+                    } else if (strcmp(users[pos].cargo, "atendente") == 0) {
                         menuAtendente(pos);
                     } 
                 }
@@ -46,46 +46,17 @@ void menuInicial() {
     } while (opc != 0);
 }
 
-void menuAtendente(int posUser) {
+void menuAtendente(int pos) {
     int opc;
 
     do {
-        printf("\n\n MENU ATENDENTE \n");
-        printf("1 - Ver pedidos\n");
-        printf("2 - Ver cardapio\n");
-        printf("0 - Sair\n");
-        printf("Escolha: ");
-        scanf("%d", &opc);
-        getchar(); 
-
-        switch (opc) {
-            case 1:
-                listarPedidos();
-                break;
-
-            case 2:
-                mostrarCardapio();
-                break;
-
-            case 0:
-                printf("Saindo do menu atendente...\n");
-                break;
-
-            default:
-                printf("Opcao invalida!\n");
-        }
-
-    } while (opc != 0);
-}
-
-void menuGerente(int posUser) {
-    int opc;
-
-    do {
-        printf("\n\n MENU GERENTE \n");
-        printf("1 - Cadastrar pizza\n");
-        printf("2 - Ver cardapio\n");
-        printf("3 - Ver pedidos\n");
+        printf("\n===== MENU ATENDENTE =====\n");
+        printf("1 - Cadastrar Cliente\n");
+        printf("2 - Listar Clientes\n");
+        printf("3 - Fazer Pedido\n");
+        printf("4 - Listar Pedidos\n");
+        printf("5 - Editar Pedido\n");
+        printf("6 - Excluir Pedido\n");
         printf("0 - Sair\n");
         printf("Escolha: ");
         scanf("%d", &opc);
@@ -93,19 +64,31 @@ void menuGerente(int posUser) {
 
         switch (opc) {
             case 1:
-                cadastrarPizza();
+                cadastrarCliente();
                 break;
 
             case 2:
-                mostrarCardapio();
+                listarClientes();
                 break;
 
             case 3:
+                cadastrarPedido();
+                break;
+
+            case 4:
                 listarPedidos();
                 break;
 
+            case 5:
+                editarPedido();
+                break;
+
+            case 6:
+                excluirPedido();
+                break;
+
             case 0:
-                printf("Saindo do menu gerente...\n");
+                printf("Voltando ao menu anterior...\n");
                 break;
 
             default:
@@ -114,3 +97,141 @@ void menuGerente(int posUser) {
 
     } while (opc != 0);
 }
+
+
+void menuGerente() {
+    int opc;
+
+    do {
+        printf("\n\n MENU GERENTE \n");
+        printf("1 - Gerenciar Usuarios\n");
+        printf("2 - Gerenciar Clientes\n");
+        printf("3 - Gerenciar Pizzas\n");
+        printf("4 - Gerenciar Pedidos\n");
+        printf("5 - Relatorios\n");
+        printf("0 - Sair\n");
+        printf("Escolha: ");
+        scanf("%d", &opc);
+        getchar();
+
+        switch (opc) {
+
+            case 1: {
+                int ou;
+                do {
+        printf("\n\n--- GERENCIAR USUARIOS ---\n");
+        printf("1 - Cadastrar Usuario\n");
+        printf("2 - Listar Usuarios\n");
+        printf("3 - Editar Usuario\n");
+        printf("4 - Excluir Usuario\n");
+        printf("0 - Voltar\n");
+        printf("Escolha: ");
+        scanf("%d", &ou);
+        getchar();
+
+        switch (ou) {
+            case 1:
+                cadastrarUser();
+                break;
+
+            case 2:
+                listarUsers();
+                break;
+
+            case 3:
+                editarUser();
+                break;
+
+            case 4:
+                excluirUser();
+                break;
+
+            case 0:
+                printf("Voltando...\n");
+                break;
+
+            default:
+                printf("Opcao invalida!\n");
+        }
+
+    } while (ou != 0);
+            } break;
+
+            case 2: {
+                int oc;
+                do {
+                    printf("\n--- GERENCIAR CLIENTES ---\n");
+                    printf("1 - Cadastrar Cliente\n");
+                    printf("2 - Listar Clientes\n");
+                    printf("0 - Voltar\n");
+                    printf("Escolha: ");
+                    scanf("%d", &oc);
+                    getchar();
+
+                    if (oc == 1) cadastrarCliente();
+                    else if (oc == 2) listarClientes();
+
+                } while (oc != 0);
+            } break;
+
+            case 3: {
+                int op;
+                do {
+                    printf("\n--- GERENCIAR PIZZAS ---\n");
+                    printf("1 - Cadastrar Pizza\n");
+                    printf("2 - Listar Pizzas\n");
+                    printf("3 - Editar Pizza\n");
+                    printf("4 - Excluir Pizza\n");
+                    printf("0 - Voltar\n");
+                    printf("Escolha: ");
+                    scanf("%d", &op);
+                    getchar();
+
+                    switch (op) {
+                        case 1: cadastrarPizza(); break;
+                        case 2: listarPizzas(); break;
+                        case 3: editarPizza(); break;
+                        case 4: excluirPizza(); break;
+                    }
+
+                } while (op != 0);
+            } break;
+
+            case 4: {
+                int op;
+                do {
+                    printf("\n--- GERENCIAR PEDIDOS ---\n");
+                    printf("1 - Fazer Pedido\n");
+                    printf("2 - Listar Pedidos\n");
+                    printf("3 - Editar Pedido\n");
+                    printf("4 - Excluir Pedido\n");
+                    printf("0 - Voltar\n");
+                    printf("Escolha: ");
+                    scanf("%d", &op);
+                    getchar();
+
+                    switch (op) {
+                        case 1: cadastrarPedido(); break;
+                        case 2: listarPedidos(); break;
+                        case 3: editarPedido(); break;
+                        case 4: excluirPedido(); break;
+                    }
+
+                } while (op != 0);
+            } break;
+
+            case 5:
+                gerarRelatorios();
+                break;
+
+            case 0:
+                printf("Voltando ao menu anterior...\n");
+                break;
+
+            default:
+                printf("Opcao invalida!\n");
+        }
+
+    } while (opc != 0);
+}
+
--- pizzafony.c
+++ pizzafony.c
@@ -4,17 +4,19 @@
 #include "structs.h"
 #include "dados.h"
 #include "menu.h"
+#include "arquivos.h"
 
 int main() {
 
     // powershell:   
-    //    gcc pizzafony.c dados.c user.c menu.c pizza.c pedidos.c -o pizzafony
+    //    gcc pizzafony.c dados.c user.c menu.c pizza.c pedidos.c clientes.c arquivos.c -o pizzafony
     //   ./pizzafony
 
-    /*git add .
-    git commit -m "texto"
-    git push */
     
+    carregarUsers();
+    carregarClientes();
+    carregarPizzas();
+    carregarPedidos();
     menuInicial();
 
     return 0;
